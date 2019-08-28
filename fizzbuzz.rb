ee483f8827c45fe0a7cd1ca146c2e8d6b73c6c0a
Vik Vangala <vvang044@gmail.com>
2019-08-28 12:11:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,6 +4,6 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
-  if int % 5 == 0 # if the number int is divisble by 5
+  elsif int % 5 == 0 # if the number int is divisble by 5
     "Buzz" # Go Buzz
 end
